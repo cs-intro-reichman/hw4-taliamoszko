e78d681635fd22756d727e7760848e6ab643ef76
taliamoszko <taliamoszko@gmail.com>
2024-12-05 10:41:46 +0200

    first version

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -24,8 +24,6 @@ public class ArrCharOps {
         System.out.println(hashCode(arr2));
     }
 
-    /** Prints the given array of characters, and moves the cursor to the next line.
-     */
     public static void println(char[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]);
@@ -33,100 +31,73 @@ public class ArrCharOps {
         System.out.println();
     }
 
-    /** Returns the char value at the specified index. Assume that the array is non-empty.
-     */
     public static char charAt(char[] arr, int index) {
-        // Replace the following statement with your code
-        return 0;
+        return arr[index];
     }
 
-    /** If the two arrays have the same value in every index, 
-     *  returns true; Otherwise returns false.
-     */
     public static boolean equals(char[] arr1, char[] arr2) {
-        // Replace the following statement with your code
-        return false;
+        if (arr1.length != arr2.length) return false;
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) return false;
+        }
+        return true;
     }
 
-    /** Returns the index within the given array of the first occurrence of the given character.
-     *  If no such character is found, returns -1.
-     */
     public static int indexOf(char[] arr, char ch) {
-        // Replace the following statement with your code
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == ch) return i;
+        }
         return -1;
     }
 
-    /** Same as indexOf(char[], char), but starts the search in the given index.
-     */
     public static int indexOf(char[] arr, char ch, int fromIndex) {
-        // Replace the following statement with your code
+        for (int i = fromIndex; i < arr.length; i++) {
+            if (arr[i] == ch) return i;
+        }
         return -1;
     }
 
-    /** Returns the index within the given arr of the last occurrence of the given character.
-     *  If no such character is found, returns -1.
-     */
     public static int lastIndexOf(char[] arr, char ch) {
-        // Replace the following statement with your code
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] == ch) return i;
+        }
         return -1;
     }
 
-    /* Returns an array which is the concatanation of the two given arrays.
-    */
     public static char[] concat(char[] arr1, char[] arr2) {
-        // Replace the following statement with your code
-        return null;
+        char[] result = new char[arr1.length + arr2.length];
+        System.arraycopy(arr1, 0, result, 0, arr1.length);
+        System.arraycopy(arr2, 0, result, arr1.length, arr2.length);
+        return result;
     }
 
-    /** Returns a new array that can be described as a sub-array of this array.
-     *  The sub-array begins at the specified beginIndex and extends to the character at index endIndex - 1.
-     *  For example, if arr contains the characters "hamburger", then subArray(4, 8) returns an array of
-     *  characters containing the characters "urge".
-     */     
     public static char[] subArray(char[] arr, int beginIndex, int endIndex) {
-        // Replace the following statement with your code
-        return null;
+        char[] result = new char[endIndex - beginIndex];
+        System.arraycopy(arr, beginIndex, result, 0, endIndex - beginIndex);
+        return result;
     }
 
-     /** Returns a single integer that represents the given array. This integer is sometimes 
-     *  referred to as the array's "hash code". Later in the course we'll explain what these 
-     *  hash codes are used for. For now, simply implement the specification given below.
-     *  The hash code is computed as: arr[0]*7^(n-1) + arr[1]*7^(n-2) + ... + arr[n-2]*7 + arr[n-1]
-     *  where arr[i] is the i'th character of the array, and n is the array's length.
-     *  The hash value of an empty array is zero.
-     */
     public static long hashCode(char[] arr) {
-        // Replace the following statement with your code
-        return 0;
+        long hash = 0;
+        long power = 1; 
+        for (int i = arr.length - 1; i >= 0; i--) {
+            hash += arr[i] * power;
+            power *= 7;
+        }
+        return hash;
     }
 
-    /**
-     * Compares the two strings lexicographically.
-     * Assume that both strings are not empty.
-     * 
-     * Characters are compared one by one from left to right, using their numeric Unicode values,
-        as follows:
-     * 1. If two characters at the same position in both strings are different,
-     *    the string with the smaller character is considered lexicographically smaller.
-     * 2. If all characters in the shorter string match the corresponding characters
-     *    in the longer string, the shorter string is considered lexicographically smaller.
-     * 3. If both strings have the same characters and the same length, they are considered equal.
-     * 
-     * Examples:
-     * - "apple" is less than "banana" because 'a' comes before 'b'.
-     * - "abc" is less than "abcd" because it is shorter.
-     * - "hello" is equal to "hello".
-     * - "date" is greater than "dark" because 't' comes after 'k'.
-     * 
-     * @param str1 the first string to compare
-     * @param str2 the second string to compare
-     * @return -1 if str1 is lexicographically less than str2,
-     *         zero if they are equal, and 1 if str1 is
-     *         lexicographically greater than str2.
-     *         return -2 if there is an error with the input.
-     */
     public static int compareTo(String str1, String str2) {
-        // Replace the following statement with your code
-        return 0;
+        int len1 = str1.length();
+        int len2 = str2.length();
+        int minLen = Math.min(len1, len2);
+
+        for (int i = 0; i < minLen; i++) {
+            if (str1.charAt(i) != str2.charAt(i)) {
+                return str1.charAt(i) < str2.charAt(i) ? -1 : 1;
+            }
+        }
+        if (len1 == len2) return 0;
+        return len1 < len2 ? -1 : 1;
     }
 }
